9af56ff1ce889ac0a15d033c635d7bb6790d3a31
Francesco Esposito <33671357+franjsco@users.noreply.github.com>
2021-10-19 22:31:46 +0200

    Initial version

---
+++ src/Client.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Franjsco\Bitcoinfees;
+
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\RequestException;
+
+class Client
+{
+    private $httpClient;
+    protected const BASE_URI = 'https://bitcoinfees.earn.com/api/v1/fees/recommended';
+
+    public function __construct()
+    {
+        $this->httpClient = new GuzzleClient(['base_uri' => self::BASE_URI]);
+    }
+
+
+    public function getHttpClient(): GuzzleClient
+    {
+        return $this->httpClient;
+    }
+
+
+    public function getData() 
+    {   
+        try {
+            $response = $this->httpClient->request('GET');
+            $body = (string) $response->getBody();
+
+            return $this->transformData($body);
+        } catch (RequestException $e) {
+            return [];
+        }
+       
+    }
+
+
+    private function transformData($jsonData)
+    {
+        $data = json_decode($jsonData, true);
+        return $data;
+    }
+
+
+}
